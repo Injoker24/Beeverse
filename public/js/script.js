97463f529c773143183c6e527e27223f75d1cec9
Injoker24 <mcl2411@gmail.com>
2022-08-09 16:49:54 +0700

    fix js

--- public/js/script.js
+++ public/js/script.js
@@ -9,10 +9,10 @@ $(document).ready(function() {
         if (x < max_fields) {
             x++;
             if(x == 4){
-                $(wrapper).append('<div><div class="form-group"><input type="text" class="form-control" id="interest4" name="interest4" placeholder="Enter work interest 4..."></div><div class="custom-file mb-1"><input type="file" id="interest_image4" name="interest_image4" accept="image/png, image/jpeg"></div><div class="button-danger delete-button mb-3" style="width:100px;">Remove</div></div>');
+                $(wrapper).append('<div><div class="form-group"><input type="text" class="form-control interest4" id="interest4" name="interest4" placeholder="Enter work interest 4..."></div><div class="custom-file mb-1"><input type="file" id="interest_image4" name="interest_image4" accept="image/png, image/jpeg"></div><div class="button-danger delete-button mb-3" style="width:100px;">Remove</div></div>');
             }
             else if (x == 5){
-                $(wrapper).append('<div><div class="form-group"><input type="text" class="form-control" id="interest5" name="interest5" placeholder="Enter work interest 5..."></div><div class="custom-file mb-1"><input type="file" id="interest_image5" name="interest_image5" accept="image/png, image/jpeg"></div><div class="button-danger delete-button mb-3" style="width:100px;">Remove</div></div>');
+                $(wrapper).append('<div><div class="form-group"><input type="text" class="form-control interest5" id="interest5" name="interest5" placeholder="Enter work interest 5..."></div><div class="custom-file mb-1"><input type="file" id="interest_image5" name="interest_image5" accept="image/png, image/jpeg"></div><div class="button-danger delete-button mb-3" style="width:100px;">Remove</div></div>');
             }
         } else {
             alert('You Reached the limits')
@@ -21,7 +21,12 @@ $(document).ready(function() {
 
     $(wrapper).on("click", ".delete-button", function(e) {
         e.preventDefault();
-        $(this).parent('div').remove();
+        if(x == 4){
+            $('.interest4').parent('div').parent('div').remove();
+        }
+        else{
+            $('.interest5').parent('div').parent('div').remove();
+        }
         x--;
     })
 });
